322a7638f9672a081cfb83e6296461b1b2726f2d
Mehdi <Mehdi0xC@gmail.com>
2020-09-15 04:58:59 +0430

    updated chronometer

--- utils.cpp
+++ utils.cpp
@@ -3,8 +3,19 @@
 #include <chrono>
 
 
-Utils::Utils()
+Chronometer::Chronometer()
 {
 	start = 0;
     end = 0;
+}
+
+void Chronometer::tic()
+{
+	start = Clock::now();
+}
+int Chronometer::toc()
+{
+	end = Clock::now();
+	elapsed = std::chrono::duration_cast<milliseconds>(end - start);
+	return elapsed;
 }
