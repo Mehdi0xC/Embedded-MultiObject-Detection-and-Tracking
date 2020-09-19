a461edd699040b4c88f59b950f3ed3b4b3fb006a
Mehdi <Mehdi0xC@gmail.com>
2020-09-19 05:50:42 +0430

    commented the code

--- config.cpp
+++ config.cpp
@@ -8,22 +8,20 @@ Config::Config()
 	////////////////////////////////////////
 	////////////////////////////////////////
 
-	// Colors for texts and tags in labels
+	// Text colors for labels
 	labelColor = cv::Scalar(250, 250, 250);
-	// Font size of texts and tags
-	fontSize = 0.64;
-	// Output window width and height
-	// NOTE: COMMENT THESE AND CORRESPONDING LINES IN MAIN IF YOU'RE GONNA .....
-	// ..... IMPLEMENT THE CODE ON RASPBERRY PI (OR IT WILL MESS UP CAMERA OUTPUT)
-	outputWindowWidth = 1000;
-	outputWindowHeight = 600;
-	// Output window width and height
-	font = cv::FONT_HERSHEY_SIMPLEX;
-	boxThickness = 2;
-	labelThickness = 2;
-	// model = ssd_mobilenet_v3
-	// model = mask_rcnn_inception
 
+	// Font size of labels
+	fontSize = 0.64;
+
+	// Label font
+	font = cv::FONT_HERSHEY_SIMPLEX;
+
+	// Box Thickness
+	boxThickness = 2;
+
+	// Label Thickness
+	labelThickness = 2;
 
 	////////////////////////////////////////
 	////////////////////////////////////////
@@ -31,14 +29,14 @@ Config::Config()
 	////////////////////////////////////////
 	////////////////////////////////////////
 
-	modelConfiguration = "models/ssd_mobilenet_v3/model_configurations.pbtxt";
-	modelWeights = "models/ssd_mobilenet_v3/frozen_graph.pb";
-	modelSize = 320;
+	// NOTE: COMMENT THESE AND CORRESPONDING LINES IN MAIN IF YOU'RE GONNA .....
+	// ..... IMPLEMENT THE CODE ON RASPBERRY PI (OR IT WILL MESS UP CAMERA OUTPUT)
+	
+	// Output window width
 	outputWindowWidth = 1000;
-	outputWindowHeight = 600;
-	trackerType = "MOSSE";
-	detectionRate = 5;
-	// trackerType = "KCF";
+
+	// Output window height
+	outputWindowHeight = 600;	
 
 	////////////////////////////////////////
 	////////////////////////////////////////
@@ -46,7 +44,17 @@ Config::Config()
 	////////////////////////////////////////
 	////////////////////////////////////////
 
+	// Model configuration address
+	modelConfiguration = "models/ssd_mobilenet_v3/model_configurations.pbtxt";
 
+	// Model weights address
+	modelWeights = "models/ssd_mobilenet_v3/frozen_graph.pb";
+
+	// Model input address
+	modelSize = 320;
+
+	// Specifies after how many frame processing iterations you want to call the model for prediction
+	detectionRate = 5;
 
 	////////////////////////////////////////
 	////////////////////////////////////////
@@ -54,8 +62,13 @@ Config::Config()
 	////////////////////////////////////////
 	////////////////////////////////////////
 
+	// Controls the confidence threshold of the prediction model 
 	confidenceThreshold = 0.5;
+
+	// Controls the tracker re-identification mechanism
 	intersectionThreshold = 0.5;
+
+	// Controls the tracker merging mechanism 
 	trackerMergeThreshold = 0.7;
 
 	////////////////////////////////////////
@@ -81,15 +94,20 @@ Config::Config()
 	indices.push_back(3);
 	colors.push_back(cv::Scalar(0, 0, 255));
 
+	// Motorcycle class detection index and color
 	classes.push_back("motorcycle");
 	indices.push_back(4);
 	colors.push_back(cv::Scalar(255, 0, 255));
 
+	// Bus class detection index and color
 	classes.push_back("bus");
 	indices.push_back(6);
 	colors.push_back(cv::Scalar(255, 211, 0));
 
+	// Truck class detection index and color
 	classes.push_back("truck");
 	indices.push_back(8);
 	colors.push_back(cv::Scalar(0, 0, 255));
+
+	// NOTE: YOU CAN ADD OTHER CLASSES OF COCO DATASET (UP TO 80 CLASSES)
 }
