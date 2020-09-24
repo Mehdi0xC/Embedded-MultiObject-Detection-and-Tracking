586c3974a47ddc6b4cf6ed7e1bf8c2cfd5b4f8c6
Mehdi <Mehdi0xC@gmail.com>
2020-09-24 18:55:44 +0330

    update

--- local_run.sh
+++
@@ -1,1 +0,0 @@
-g++ `pkg-config --cflags --libs /Users/mehdi/mlib/opencv/build/unix-install/opencv4.pc` -std=c++11  *.cpp && ./a.out
--- local_run2.sh
+++
@@ -1,2 +0,0 @@
-g++ `pkg-config --cflags --libs /usr/local/Cellar/opencv/4.4.0_2/lib/pkgconfig/opencv4.pc` -std=c++11  *.cpp && ./a.out
-
